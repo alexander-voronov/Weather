6c9c25b2f88a29fe31779c69876df5dedd8a7995
Alexander Voronov <dr.strum@gmail.com>
2020-10-19 22:37:05 +0300

    HW_4_19/10/2020

--- app/src/main/java/ru/geekbrains/weather/MainActivity.java
+++ app/src/main/java/ru/geekbrains/weather/MainActivity.java
@@ -1,95 +1,50 @@
 package ru.geekbrains.weather;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Constants {
 
-    private static final String TAG = "MyActivity";
-
-    private TextView textCounter;
-
-    private int counter = 0;
+    private final static int REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button startSecondActivity = findViewById(R.id.button_1);
+        startSecondActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "MOSCOW", Toast.LENGTH_SHORT).show();
+
+                EditText txt = findViewById(R.id.textView_1);
+
+                Parcel parcel = new Parcel();
+
+                parcel.text = txt.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra(TEXT, parcel);
+                startActivityForResult(intent, REQUEST_CODE);
+
             }
         });
-
-        Button button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "KAZAN", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        textCounter = findViewById(R.id.textCounter);
-        final MainPresenter presenter = MainPresenter.getInstance();
-        textCounter.setText(((Integer)presenter.getCounter()).toString());
-
-
-                //setText(((Integer)presenter.getCounter()).toString());
-
-        Button button3 = findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                presenter.incrementCounter();
-                textCounter.setText(((Integer)presenter.getCounter()).toString());
-            }
-        });
-
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != REQUEST_CODE) {
+        super.onActivityResult(requestCode, resultCode, data);
+        return;
+        }
+        if (resultCode == RESULT_OK){
+            EditText num = findViewById(R.id.textView_3);
+        }
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-    }
-
 }
---
+++ app/src/main/java/ru/geekbrains/weather/Parcel.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.weather;
+
+import java.io.Serializable;
+
+public class Parcel implements Serializable {
+    public String text;
+}
